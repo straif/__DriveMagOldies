e2fcb3cea81f71036544edf22d07d827d64b45c9
Andrei Gusty Streitfelder <von33besten@yahoo.com>
2017-03-27 14:01:50 +0300

    completed most of directory structure to be used in angular-routing's "templateUrl" property name

--- app.js
+++ app.js
@@ -12,7 +12,59 @@
                 templateUrl: './templates/second.html',
                 controller: 'MainController'
             })
+            //todo w. route params --- see comments down!!! ===============================
+            .when('/ww/brand-01/current-model-01', {
+                templateUrl: './templates/brands/brand-01/current-model-01.html',
+                controller: 'MainController'
+            })
+            .when('/ww/brand-01/current-model-02', {
+                templateUrl: './templates/brands/brand-01/current-model-02.html',
+                controller: 'MainController'
+            })
+            .when('/ww/brand-01/current-model-03', {
+                templateUrl: './templates/brands/brand-01/current-model-03.html',
+                controller: 'MainController'
+            })
+            // ...
+            //until route params, same for brand-02 ... 07 ================================
+            //=============================================================================
+            //todo w. route params --- see comments down!!! ===============================
+            .when('/ww/car-file-01', {
+                templateUrl: './templates/car-files/car-file-01.html',
+                controller: 'MainController'
+            })
+            // ...
+            //until route params, same for car-files-02 ... 07 ============================
+            //=============================================================================
+            //todo w. route params --- see comments down!!! ===============================
+            .when('/ww/news-01', {
+                templateUrl: './templates/news/news-01.html',
+                controller: 'MainController'
+            })
+            // ...
+            //until route params, same for car-files-02 ... 07 ============================
             .otherwise({redirectTo: '/main'});
     });
     
 }());
+
+/*
+ * .when('/ww/:brand/:current-model', {
+                templateUrl: './templates/brands/:brand/:current-model.html',
+                controller: 'MainController'
+            })
+ */
+
+/*
+ * .when('/ww/:car-file', {
+                templateUrl: './templates/car-files/:car-file.html',
+                controller: 'MainController'
+            })
+ */
+
+/*
+ * .when('/ww/:news', {
+                templateUrl: './templates/news/:news.html',
+                controller: 'MainController'
+            })
+ */
