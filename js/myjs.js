53bb0de6408457b48ec9872028c95ccee748c6f3
Andrei Gusty Streitfelder <von33besten@yahoo.com>
2017-04-17 13:19:45 +0300

    (in the last commit) removed the blurry logo problem - css stuff
    added and adapted in the left-column the left-find-cars div (form, select)
     & left-find-cars-submit button - detected & included the JS functionality
    (in myjs.js), added the arrow-down.png (css stuff)

--- js/myjs.js
+++ js/myjs.js
@@ -2,3 +2,11 @@ var f = $("#page-banner").html();
 $("#page-title").html(f), $("#car-brands").hide(), $("#toggle-brands").click(function(t) {
 	t.preventDefault(), $("#car-brands").slideToggle(200)
 }); 
+
+$(window).bind("pageshow", function() {
+        $(".left-form").find("select").val("")
+});
+
+$("#left-find-cars-region-selector").change(function() {
+        "" != $(this).val() && $("#left-find-cars-submit").removeClass("btn-disabled")
+});
