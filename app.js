e1e563cd733182b604bab9ab46b0860abd0d7eed
Andrei Gusty Streitfelder <von33besten@yahoo.com>
2017-04-19 19:04:26 +0300

    added the rest of html-anchor/angular-routes (!however will have to implement it more elegant with route params)

--- app.js
+++ app.js
@@ -25,6 +25,62 @@
                 templateUrl: './templates/brands/brand-aston/current-model-aston.html',
                 controller: 'MainController'
             })
+            .when('/ww/brand-benz/current-model-benz', {
+                templateUrl: './templates/brands/brand-benz/current-model-benz.html',
+                controller: 'MainController'
+            })
+            .when('/ww/brand-bugatti/current-model-bugatti', {
+                templateUrl: './templates/brands/brand-bugatti/current-model-bugatti.html',
+                controller: 'MainController'
+            })
+            .when('/ww/brand-buick/current-model-buick', {
+                templateUrl: './templates/brands/brand-buick/current-model-buick.html',
+                controller: 'MainController'
+            })
+            .when('/ww/brand-cady/current-model-cady', {
+                templateUrl: './templates/brands/brand-cady/current-model-cady.html',
+                controller: 'MainController'
+            })
+            .when('/ww/brand-chevy/current-model-chevy', {
+                templateUrl: './templates/brands/brand-chevy/current-model-chevy.html',
+                controller: 'MainController'
+            })
+            .when('/ww/brand-fiat/current-model-fiat', {
+                templateUrl: './templates/brands/brand-fiat/current-model-fiat.html',
+                controller: 'MainController'
+            })
+            .when('/ww/brand-ford/current-model-ford', {
+                templateUrl: './templates/brands/brand-ford/current-model-ford.html',
+                controller: 'MainController'
+            })
+            .when('/ww/brand-gmc/current-model-gmc', {
+                templateUrl: './templates/brands/brand-gmc/current-model-gmc.html',
+                controller: 'MainController'
+            })
+            .when('/ww/brand-lancia/current-model-lancia', {
+                templateUrl: './templates/brands/brand-lancia/current-model-lancia.html',
+                controller: 'MainController'
+            })
+            .when('/ww/brand-peugeot/current-model-peugeot', {
+                templateUrl: './templates/brands/brand-peugeot/current-model-peugeot.html',
+                controller: 'MainController'
+            })
+            .when('/ww/brand-renault/current-model-renault', {
+                templateUrl: './templates/brands/brand-renault/current-model-renault.html',
+                controller: 'MainController'
+            })
+            .when('/ww/brand-rolls/current-model-rolls', {
+                templateUrl: './templates/brands/brand-rolls/current-model-rolls.html',
+                controller: 'MainController'
+            })
+            .when('/ww/brand-skoda/current-model-skoda', {
+                templateUrl: './templates/brands/brand-skoda/current-model-skoda.html',
+                controller: 'MainController'
+            })
+            .when('/ww/brand-vauxhall/current-model-vauxhall', {
+                templateUrl: './templates/brands/brand-vauxhall/current-model-vauxhall.html',
+                controller: 'MainController'
+            })
             // ... dar vezi mai jos! la un moment dat sa folosest route params!
             //si sa faci controllere pt. fiecare
             
