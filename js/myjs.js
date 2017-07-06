87adfb5fb41c33de84ec05e9e229eaaaba86d9c7
Andrei Gusty Streitfelder <von33besten@yahoo.com>
2017-07-06 18:50:16 +0300

    styled the "Search by car brand" list & btn

--- js/myjs.js
+++ js/myjs.js
@@ -1,6 +1,11 @@
 var f = $("#page-banner").html();
-$("#page-title").html(f), $("#car-brands").hide(), $("#toggle-brands").click(function(t) {
-	t.preventDefault(), $("#car-brands").slideToggle(200);
+$("#page-title").html(f);
+ 
+$("#car-brands").hide(); 
+
+$("#toggle-brands").click(function(t) {
+	t.preventDefault(); 
+	$("#car-brands").slideToggle(200);
 }); 
 
 $(window).bind("pageshow", function() {
